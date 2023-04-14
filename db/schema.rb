027e8b16bbd3281c3ba0bf6dfa1e5d2585dff1da
Andrew Caruso <10090913+Yzma@users.noreply.github.com>
2023-04-14 15:49:26 -0400

    Created Platforms model with migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_04_14_194810) do
+ActiveRecord::Schema.define(version: 2023_04_14_194911) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,12 @@ ActiveRecord::Schema.define(version: 2023_04_14_194810) do
     t.datetime "releaseDate"
     t.decimal "avg_score", precision: 3, scale: 1
     t.integer "total_rating"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "platforms", force: :cascade do |t|
+    t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
---
+++ spec/factories/platforms.rb
@@ -0,0 +1,5 @@
+FactoryBot.define do
+  factory :platform do
+    name { "MyString" }
+  end
+end
