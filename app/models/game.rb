8920b3da9c66d872a5352990eccd8c7d456b9d71
javascriptsucks <evix1988@live.cn>
2023-04-14 15:03:09 -0600

    fix many to many relationship

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,8 +1,9 @@
 class Game < ApplicationRecord
   has_many :user_games
-  has_many :tags
-  has_many :platforms
-  has_many :genres
+  has_and_belongs_to_many :genres
+  has_and_belongs_to_many :tags
+  has_and_belongs_to_many :platforms
+
 
   has_many :users, through: :user_games
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,40 +6,40 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-# 25.times do
-#   user = User.create(
-#     username: Faker::Name.name,
-#     email: Faker::Internet.email,
-#     password_digest: "password",
-#     banner_picture: Faker::LoremFlickr.image(size: "500x300", search_terms: ['game']), user_picture: Faker::LoremFlickr.image(size: "300x300",
-#     search_terms: ['game']),
-#     bio: Faker::Lorem.paragraph(sentence_count: 2),
-#     is_active: true
-#     )
+25.times do
+  user = User.create(
+    username: Faker::Name.name,
+    email: Faker::Internet.email,
+    password_digest: "password",
+    banner_picture: Faker::LoremFlickr.image(size: "500x300", search_terms: ['game']), user_picture: Faker::LoremFlickr.image(size: "300x300",
+    search_terms: ['game']),
+    bio: Faker::Lorem.paragraph(sentence_count: 2),
+    is_active: true
+    )
 
-#   game = Game.create(
-#     name: Faker::Game.title,
-#     description: Faker::Lorem.paragraph(sentence_count: 2),
-#     imageURL: Faker::LoremFlickr.image(size: "300x300", search_terms: ['game']),
-#     releaseDate: Faker::Date.between(from: '2014-09-23', to: '2021-09-25'),
-#     avg_score: Faker::Number.decimal(l_digits: 1, r_digits: 1),
-#     total_rating: Faker::Number.between(from: 1, to: 100)
-#   )
-#   2.times do
-#     game.genres.create(name: Faker::Game.genre)
-#     game.platforms.create(name: Faker::Game.platform)
-#     game.tags.create(name: Faker::Game.genre)
-#   end
+  game = Game.create(
+    name: Faker::Game.title,
+    description: Faker::Lorem.paragraph(sentence_count: 2),
+    imageURL: Faker::LoremFlickr.image(size: "300x300", search_terms: ['game']),
+    releaseDate: Faker::Date.between(from: '2014-09-23', to: '2021-09-25'),
+    avg_score: Faker::Number.decimal(l_digits: 1, r_digits: 1),
+    total_rating: Faker::Number.between(from: 1, to: 100)
+  )
+  2.times do
+    game.genres.create(name: Faker::Game.genre)
+    game.platforms.create(name: Faker::Game.platform)
+    game.tags.create(name: Faker::Game.genre)
+  end
 
-  # user_game = UserGame.create(
-  # user_id: user.id,
-  # game_id: game.id,
-  # game_status: "Planning", game_note: Faker::Lorem.paragraph(sentence_count: 2),
-  # start_date: Faker::Date.between(from: '2014-09-23', to: '2021-09-25'),
-  # completed_date: Faker::Date.between(from: '2014-09-23', to: '2021-09-25'),
-  # private: Faker::Boolean.boolean(true_ratio: 0.5),
-  # rating: Faker::Number.between(from: 1, to: 5), review: Faker::Lorem.paragraph(sentence_count: 2)
-  # )
+  user_game = UserGame.create(
+  user_id: user.id,
+  game_id: game.id,
+  game_status: "Planning", game_note: Faker::Lorem.paragraph(sentence_count: 2),
+  start_date: Faker::Date.between(from: '2014-09-23', to: '2021-09-25'),
+  completed_date: Faker::Date.between(from: '2014-09-23', to: '2021-09-25'),
+  private: Faker::Boolean.boolean(true_ratio: 0.5),
+  rating: Faker::Number.between(from: 1, to: 5), review: Faker::Lorem.paragraph(sentence_count: 2)
+  )
 
 
-# end
+end
