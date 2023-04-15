36b1c3df596c2b9f5b272beb21d3f2b840b681f2
Viet Tran <viettran101294@gmail.com>
2023-04-15 15:50:54 -0400

    Add test for get all games

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,19 +17,19 @@ end
     username: Faker::Name.name,
     email: Faker::Internet.email,
     password_digest: "password",
-    banner_picture: Faker::LoremFlickr.image(size: "500x300", search_terms: ['game']), user_picture: Faker::LoremFlickr.image(size: "300x300",
-    search_terms: ['game']),
+    banner_picture: Faker::LoremFlickr.image(size: "500x300", search_terms: ["game"]), user_picture: Faker::LoremFlickr.image(size: "300x300",
+                                                                                                                              search_terms: ["game"]),
     bio: Faker::Lorem.paragraph(sentence_count: 2),
-    is_active: true
-    )
+    is_active: true,
+  )
 
   game = Game.create(
     name: Faker::Game.title,
     description: Faker::Lorem.paragraph(sentence_count: 2),
-    imageURL: Faker::LoremFlickr.image(size: "300x300", search_terms: ['game']),
-    releaseDate: Faker::Date.between(from: '2014-09-23', to: '2021-09-25'),
+    imageURL: Faker::LoremFlickr.image(size: "300x300", search_terms: ["game"]),
+    releaseDate: Faker::Date.between(from: "2014-09-23", to: "2021-09-25"),
     avg_score: Faker::Number.decimal(l_digits: 1, r_digits: 1),
-    total_rating: Faker::Number.between(from: 1, to: 100)
+    total_rating: Faker::Number.between(from: 1, to: 100),
   )
   2.times do
     game.genres << Genre.all.sample
@@ -38,14 +38,12 @@ end
   end
 
   user_game = UserGame.create(
-  user_id: user.id,
-  game_id: game.id,
-  game_status: "Planning", game_note: Faker::Lorem.paragraph(sentence_count: 2),
-  start_date: Faker::Date.between(from: '2014-09-23', to: '2021-09-25'),
-  completed_date: Faker::Date.between(from: '2014-09-23', to: '2021-09-25'),
-  private: Faker::Boolean.boolean(true_ratio: 0.5),
-  rating: Faker::Number.between(from: 1, to: 5), review: Faker::Lorem.paragraph(sentence_count: 2)
+    user_id: user.id,
+    game_id: game.id,
+    game_status: "Planning", game_note: Faker::Lorem.paragraph(sentence_count: 2),
+    start_date: Faker::Date.between(from: "2014-09-23", to: "2021-09-25"),
+    completed_date: Faker::Date.between(from: "2014-09-23", to: "2021-09-25"),
+    private: Faker::Boolean.boolean(true_ratio: 0.5),
+    rating: Faker::Number.between(from: 1, to: 5), review: Faker::Lorem.paragraph(sentence_count: 2),
   )
-
-
 end
