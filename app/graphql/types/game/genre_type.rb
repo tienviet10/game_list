2cfdd7ce86991e437d9394fc3520cc4364e4e825
Viet Tran <viettran101294@gmail.com>
2023-04-15 13:30:11 -0400

    Move game related types into its folder

--- app/graphql/types/game/genre_type.rb
+++ app/graphql/types/game/genre_type.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
 module Types
-  module Tag
-    class TagType < Types::BaseObject
+  module Game
+    class GenreType < Types::BaseObject
       field :id, ID, null: false
       field :name, String, null: false
       field :games, [Types::Game::GameType], null: false
