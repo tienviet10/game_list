34144cfb7199a854143449acbe35451fd2ba01e0
javascriptsucks <evix1988@live.cn>
2023-04-16 10:19:44 -0600

    update user rspec test code pass

---
+++ spec/graphql/mutation/update_user_spec.rb
@@ -0,0 +1,34 @@
+require "rails_helper"
+
+describe Mutations::Users::UpdateUser do
+  def updateAUser(id:, payload:, action:)
+    described_class.new(object: nil, field: nil, context: {}).resolve(
+      id: id, payload: payload, action: action,
+    )
+  end
+
+  it "updates a user" do
+    user = User.create(
+      username: "Test User",
+      email: "email@example.com",
+      password: "password",
+      is_active: true,
+    )
+
+    result = updateAUser(
+      id: user.id,
+      payload: "new password",
+      action: "password",
+    )
+
+    user = result[:user]
+
+    expect(user).to be_persisted
+    expect(user.password).to eq("new password")
+    expect(user.email).to eq("email@example.com")
+    expect(user.username).to eq("Test User")
+    expect(user.is_active).to eq(true)
+  end
+end
+
+
