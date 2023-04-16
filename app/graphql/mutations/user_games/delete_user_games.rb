385d36b976d1cfa38e5f43252ab1ec1d4fba7615
javascriptsucks <evix1988@live.cn>
2023-04-15 22:49:17 -0600

    fix delete mutation edge case that game not exist

--- app/graphql/mutations/user_games/delete_user_games.rb
+++ app/graphql/mutations/user_games/delete_user_games.rb
@@ -5,12 +5,11 @@ module Mutations
       argument :user_id, ID, required: true
       argument :game_id, ID, required: true
 
-      field :user_game, Types::UserGame::UserGameType, null: false
-      field :errors, [String], null: false
+      field :user_game, Types::UserGame::UserGameType, null: true
+      field :errors, [String], null: true
 
       def resolve(user_id:, game_id:)
         user_game = UserGame.find_by(user_id: user_id, game_id: game_id)
-
         if user_game
           user_game.destroy
           {user_game: user_game, errors: []}
