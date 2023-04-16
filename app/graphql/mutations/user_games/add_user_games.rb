f4f56e108a92305a0cfecd017bfecbc69aec3e15
javascriptsucks <evix1988@live.cn>
2023-04-15 23:37:21 -0600

    git userGames default value, and prevents duplicate

--- app/graphql/mutations/user_games/add_user_games.rb
+++ app/graphql/mutations/user_games/add_user_games.rb
@@ -5,23 +5,30 @@ module Mutations
 
       argument :user_id, ID, required: true
       argument :game_id, ID, required: true
-      argument :game_status, String, required: true
-      argument :game_note, String, required: true
-      argument :start_date, String, required: true
-      argument :completed_date, String, required: true
-      argument :private, Boolean, required: true
-      argument :rating, Float, required: true
-      argument :review, String, required: true
+      # argument :game_status, String, required: true
+      # argument :game_note, String, required: true
+      # argument :start_date, String, required: true
+      # argument :completed_date, String, required: true
+      # argument :private, Boolean, required: true
+      # argument :rating, Float, required: true
+      # argument :review, String, required: true
 
-      field :user_game, Types::UserGame::UserGameType, null: false
-      field :errors, [String], null: false
+      field :user_game, Types::UserGame::UserGameType, null: true
+      field :errors, [String], null: true
 
-      def resolve(user_id:, game_id:, game_status:, game_note:, start_date:, completed_date:, private:, rating:, review:)
-        user_game = UserGame.new(user_id: user_id, game_id: game_id, game_status: game_status, game_note: game_note, start_date: start_date, completed_date: completed_date, private: private, rating: rating, review: review)
-        if user_game.save
-          {user_game: user_game, errors: []}
+      def resolve(user_id:, game_id:)
+
+        if UserGame.find_by(user_id: user_id, game_id: game_id)
+           {user_game: nil, errors: ["User Game already exists"]}
         else
-          {user_game: nil, errors: user_game.errors.full_messages}
+          user_game = UserGame.new(user_id: user_id, game_id: game_id)
+
+          if user_game.save
+            {user_game: user_game, errors: []}
+          else
+            {user_game: nil, errors: user_game.errors.full_messages}
+          end
+
         end
       end
 
