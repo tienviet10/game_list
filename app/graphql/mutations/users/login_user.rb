cbbe2967a606c0c2323665137a380eebd53db06a
Viet Tran <viettran101294@gmail.com>
2023-04-16 18:15:53 -0400

    Add jwt token to login, register, and graphql controller

--- app/graphql/mutations/users/login_user.rb
+++ app/graphql/mutations/users/login_user.rb
@@ -5,22 +5,25 @@ module Mutations
       argument :password, String, required: true
 
       field :user, Types::User::UserType, null: false
+      field :token, String, null: false
       field :errors, [String], null: false
 
       def resolve(email:, password:)
         user = User.find_by(email: email)
         if user&.authenticate(password)
+          token = JWT.encode({ user_id: user.id, exp: 7.days.from_now.to_i }, Rails.application.secrets.secret_key_base)
           {
             user: user,
+            token: token,
             errors: [],
           }
         else
-          p "Error: Invalid email or password"
           {
             user: {
               username: nil,
               email: nil,
             },
+            token: nil,
             errors: GraphQL::ExecutionError.new("Invalid email or password"),
           }
         end
--- app/graphql/mutations/users/register_user.rb
+++ app/graphql/mutations/users/register_user.rb
@@ -6,18 +6,25 @@ module Mutations
       argument :password, String, required: true
 
       field :user, Types::User::UserType, null: true
+      field :token, String, null: false
       field :errors, [String], null: true
 
       def resolve(username:, email:, password:)
         user = User.new(username: username, email: email, password: password, is_active: true)
         if user.save
+          token = JWT.encode({ user_id: user.id, exp: 7.days.from_now.to_i }, Rails.application.secrets.secret_key_base)
           {
             user: user,
+            token: token,
             errors: [],
           }
         else
           {
-            user: nil,
+            user: {
+              username: nil,
+              email: nil,
+            },
+            token: nil,
             errors: user.errors.full_messages,
           }
         end
