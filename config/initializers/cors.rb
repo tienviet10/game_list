86dd056d8cc50136a1fe85562fcb6ed15a4529e2
Viet Tran <viettran101294@gmail.com>
2023-04-20 14:55:59 -0400

    Add cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -14,3 +14,10 @@
 #       methods: [:get, :post, :put, :patch, :delete, :options, :head]
 #   end
 # end
+
+Rails.application.config.middleware.insert_before 0, Rack::Cors do
+  allow do
+    origins "http://localhost:5173", "http://127.0.0.1:5173"
+    resource "*", headers: :any, methods: [:get, :post, :options]
+  end
+end
