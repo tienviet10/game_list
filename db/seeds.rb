d6ba47054af0f325e6f8dfdaf17952ff4daaa7fa
Viet Tran <viettran101294@gmail.com>
2023-04-22 17:22:47 -0400

    Add one user that is the same accross all db:reset

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,76 +13,76 @@
 # end
 
 genres = [
-  'First-person shooter',
-  'Puzzle',
-  'Platformer',
-  'Action',
-  'Adventure',
-  'Real-time strategy',
-  'Action role-playing',
-  'Dungeon crawl',
-  'Roguelike',
-  'Tactical role-playing',
-  'Sports',
-  'Simulation',
-  'Fighting',
-  'Massively multiplayer online',
-  'Stealth',
-  'Survival',
-  'Rhythm',
-  'Survival horror',
-  'Text adventure',
-  'Visual novel',
-  'Real-time strategy',
-  'Multiplayer online battle arena',
-  'Tower defense',
-  'Trivia',
-  'Real-time tactics',
-  'Hack and slash',
-  'Battle royale',
-  'Third-person shooter',
-  'Music'
+  "First-person shooter",
+  "Puzzle",
+  "Platformer",
+  "Action",
+  "Adventure",
+  "Real-time strategy",
+  "Action role-playing",
+  "Dungeon crawl",
+  "Roguelike",
+  "Tactical role-playing",
+  "Sports",
+  "Simulation",
+  "Fighting",
+  "Massively multiplayer online",
+  "Stealth",
+  "Survival",
+  "Rhythm",
+  "Survival horror",
+  "Text adventure",
+  "Visual novel",
+  "Real-time strategy",
+  "Multiplayer online battle arena",
+  "Tower defense",
+  "Trivia",
+  "Real-time tactics",
+  "Hack and slash",
+  "Battle royale",
+  "Third-person shooter",
+  "Music",
 ]
 
 platforms = [
-  'Xbox',
-  'Xbox 360',
-  'Xbox One',
-  'PlayStation',
-  'PlayStation 2',
-  'PlayStation 3',
-  'PlayStation 4',
-  'PlayStation Portable',
-  'PlayStation Vita',
-  'Game Boy',
-  'Game Boy Color',
-  'Game Boy Advance',
-  'Nintendo DS',
-  'Nintendo DSi',
-  'Nintendo 3DS',
-  'Nintendo Entertainment System',
-  'Super Nintendo Entertainment System',
-  'Nintendo 64',
-  'GameCube',
-  'Wii',
-  'Wii U',
-  'Nintendo Switch',
-  'Windows',
-  'macOS',
-  'Linux',
-  'Sega Genesis',
-  'Sega Master System',
-  'Game Gear',
-  'Sega Saturn',
-  'Sega Dreamcast',
-  'Virtual Boy'
+  "Xbox",
+  "Xbox 360",
+  "Xbox One",
+  "PlayStation",
+  "PlayStation 2",
+  "PlayStation 3",
+  "PlayStation 4",
+  "PlayStation Portable",
+  "PlayStation Vita",
+  "Game Boy",
+  "Game Boy Color",
+  "Game Boy Advance",
+  "Nintendo DS",
+  "Nintendo DSi",
+  "Nintendo 3DS",
+  "Nintendo Entertainment System",
+  "Super Nintendo Entertainment System",
+  "Nintendo 64",
+  "GameCube",
+  "Wii",
+  "Wii U",
+  "Nintendo Switch",
+  "Windows",
+  "macOS",
+  "Linux",
+  "Sega Genesis",
+  "Sega Master System",
+  "Game Gear",
+  "Sega Saturn",
+  "Sega Dreamcast",
+  "Virtual Boy",
 ]
 
 genres.each do |genre|
   Genre.create(name: genre)
 end
 
-["Indie", "Singleplayer", 'Adventure', 'Action', '2D', '3D', 'Puzzle', 'Fantasy'].each do |tag|
+["Indie", "Singleplayer", "Adventure", "Action", "2D", "3D", "Puzzle", "Fantasy"].each do |tag|
   Tag.create(name: tag)
 end
 
@@ -125,3 +125,39 @@ end
     rating: Faker::Number.between(from: 1, to: 5), review: Faker::Lorem.paragraph(sentence_count: 2),
   )
 end
+
+user1 = User.create(
+  username: "Vv",
+  email: "v@gmail.com",
+  password: "password",
+  banner_picture: Faker::LoremFlickr.image(size: "500x300", search_terms: ["game"]), user_picture: Faker::LoremFlickr.image(size: "300x300",
+                                                                                                                            search_terms: ["game"]),
+  bio: Faker::Lorem.paragraph(sentence_count: 2),
+  is_active: true,
+)
+
+game1 = Game.create(
+  name: Faker::Game.title,
+  description: Faker::Lorem.paragraph(sentence_count: 2),
+  imageURL: Faker::LoremFlickr.image(size: "300x300", search_terms: ["game"]),
+  releaseDate: Faker::Date.between(from: "2014-09-23", to: "2021-09-25"),
+  avg_score: Faker::Number.decimal(l_digits: 1, r_digits: 1),
+  total_rating: Faker::Number.between(from: 1, to: 100),
+)
+2.times do
+  game1.genres << Genre.all.sample
+  game1.tags << Tag.all.sample
+  game1.platforms << Platform.all.sample
+end
+
+UserGame.create(
+  user_id: user1.id,
+  game_id: game1.id,
+  game_status: "Planning",
+  game_note: Faker::Lorem.paragraph(sentence_count: 2),
+  start_date: Faker::Date.between(from: "2014-09-23", to: "2021-09-25"),
+  completed_date: Faker::Date.between(from: "2014-09-23", to: "2021-09-25"),
+  private: Faker::Boolean.boolean(true_ratio: 0.5),
+  rating: Faker::Number.between(from: 1, to: 5),
+  review: Faker::Lorem.paragraph(sentence_count: 2),
+)
