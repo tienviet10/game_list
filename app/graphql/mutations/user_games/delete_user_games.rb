852283b2194ffec1d459b792ddbe5cc178c098a0
javascriptsucks <evix1988@live.cn>
2023-04-22 18:34:00 -0600

    refactor delete_userGame mutation that it get userid from token

--- app/graphql/mutations/user_games/delete_user_games.rb
+++ app/graphql/mutations/user_games/delete_user_games.rb
@@ -1,19 +1,22 @@
 module Mutations
   module UserGames
     class DeleteUserGames < Mutations::BaseMutation
-      argument :user_id, ID, required: true
       argument :game_id, ID, required: true
 
       field :user_game, Types::UserGame::UserGameType, null: true
       field :errors, [String], null: true
 
-      def resolve(user_id:, game_id:)
-        user_game = UserGame.find_by(user_id: user_id, game_id: game_id)
-        if user_game
-          user_game.destroy
-          { user_game: user_game, errors: [] }
+      def resolve(game_id:)
+        if context[:current_user].nil?
+          { user_game: nil, errors: ["Authentication required"] }
         else
-          { user_game: nil, errors: ["User Game not found"] }
+          user_game = UserGame.find_by(user_id: context[:current_user], game_id: game_id)
+          if user_game
+            user_game.destroy
+            { user_game: user_game, errors: [] }
+          else
+            { user_game: nil, errors: ["User Game not found"] }
+          end
         end
       end
     end
--- spec/graphql/mutation/delete_user_game_spec.rb
+++ spec/graphql/mutation/delete_user_game_spec.rb
@@ -1,16 +1,17 @@
 require "rails_helper"
+require "factory_bot_rails"
 
-describe Mutations::UserGames::DeleteUserGames do
+describe Mutations::UserGames::DeleteUserGames, type: :request do
   describe "delete a game in user's list" do
-    let(:user) { create(:user) }
-    let(:game) { create(:game) }
-
-    subject { described_class.new(object: nil, context: nil, field: nil) }
+    let!(:user) { create(:user, username: "user0", email: "user0@gmail.com", password: "password") }
+    let!(:game) { create(:game, name: "Persona 5", description: "Inside a casino, during one of their heists, the group known as Phantom Thieves of Hearts is cornered by the police. Unable to escape, the leader of the group (the game's protagonist) is put under arrest, and goes into interrogation.", imageURL: "https://www.mobygames.com/game/86408/persona-5/", releaseDate: "2014-09-23", avg_score: 4.5, total_rating: 1000) }
+    let!(:token) { JWT.encode({ user_id: user.id, exp: 7.days.from_now.to_i }, Rails.application.secrets.secret_key_base) }
 
     context "When the game is in the user_games table" do
       it "deletes the game from the user_games table" do
+        post "/graphql", params: { query: mutation(game.id) }, headers: { "Authorization" => "Bearer #{token}" }
         create(:user_game, user_id: user.id, game_id: game.id)
-        result = subject.resolve(user_id: user.id, game_id: game.id)
+        result = JSON.parse(response.body).deep_symbolize_keys[:data][:deleteUserGames]
         expect(result[:user_game]).to be_destroyed
 
         expect(result[:errors]).to be_empty
@@ -23,6 +24,24 @@ describe Mutations::UserGames::DeleteUserGames do
         expect(result[:user_game]).to be_nil
         expect(result[:errors]).to include("User Game not found")
       end
+    end
+
+    def mutation(gameId)
+      <<~GQL
+        mutation  {
+          addUserGames(input: {gameId: "#{gameId}"}) {
+            userGame {
+              id
+
+              game {
+                id
+                name
+              }
+            }
+            errors
+          }
+        }
+      GQL
     end
   end
 end
