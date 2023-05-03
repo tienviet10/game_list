0b66f6811fcb91d829609b630314d90c8ff3789f
Viet Tran <viettran101294@gmail.com>
2023-05-03 11:00:18 -0400

    Add endpoint to return games list for a user by status

--- app/graphql/queries/user_games/games_by_status_for_a_user.rb
+++ app/graphql/queries/user_games/games_by_status_for_a_user.rb
@@ -2,11 +2,16 @@ module Queries
   module UserGames
     class GamesByStatusForAUser < Queries::BaseQuery
       description "Get games by status for a user"
-      argument :status, String, required: false
-      type [Types::Game::GameType], null: false
+      type Types::UserGame::UserGamesByStatusType, null: false
 
-      def resolve(status:)
-        ::UserGame.where(game_status: status, user_id: context[:current_user]).map(&:game)
+      def resolve()
+        {
+          "playing" => ::UserGame.where(user_id: context[:current_user], game_status: :Playing).map(&:game),
+          "planning" => ::UserGame.where(user_id: context[:current_user], game_status: :Planning).map(&:game),
+          "completed" => ::UserGame.where(user_id: context[:current_user], game_status: :Completed).map(&:game),
+          "paused" => ::UserGame.where(user_id: context[:current_user], game_status: :Paused).map(&:game),
+          "dropped" => ::UserGame.where(user_id: context[:current_user], game_status: :Dropped).map(&:game),
+        }
       end
     end
   end
--- app/graphql/types/user_game/user_games_by_status_type.rb
+++ app/graphql/types/user_game/user_games_by_status_type.rb
@@ -3,8 +3,11 @@
 module Types
   module UserGame
     class UserGamesByStatusType < Types::BaseObject
-      field :playing, [Types::Game::GameType], null: false
-      field :planning, [Types::Game::GameType], null: false
+      field :playing, [Types::Game::GameType], null: true
+      field :planning, [Types::Game::GameType], null: true
+      field :completed, [Types::Game::GameType], null: true
+      field :paused, [Types::Game::GameType], null: true
+      field :dropped, [Types::Game::GameType], null: true
     end
   end
 end
