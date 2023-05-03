8aaf4f463283547bf8d1a24853b2b77b3c9400c1
Viet Tran <viettran101294@gmail.com>
2023-05-03 13:32:27 -0400

    Clean up the games_by_status_for_a_user_spec file

--- spec/graphql/query/games_by_status_for_a_user_spec.rb
+++ spec/graphql/query/games_by_status_for_a_user_spec.rb
@@ -58,26 +58,9 @@ describe Queries::UserGames::GamesByStatusForAUser, type: :request do
       token = JWT.encode({ user_id: user.id, exp: 7.days.from_now.to_i }, Rails.application.secrets.secret_key_base)
       post "/graphql", params: { query: queryByStatus }, headers: { "Authorization" => "Bearer #{token}" }
 
-      json_response = JSON.parse(response.body)
       p json_response["data"]["gamesByStatusForAUser"]
       expect(json_response["data"]["gamesByStatusForAUser"]).to eq({ "playing" => [{ "id" => game3.id.to_s, "name" => "Assassin's Creed", "imageURL" => "https://www.igdb.com/games/assassin-s-creed/", "avgScore" => 4.4, "platforms" => ["Xbox"] }], "planning" => [{ "id" => game1.id.to_s, "name" => "Persona 5", "imageURL" => "https://www.mobygames.com/game/86408/persona-5/", "avgScore" => 4.5, "platforms" => ["PS5", "Xbox"] }, { "id" => game2.id.to_s, "name" => "FIFA 11", "imageURL" => "https://www.mobygames.com/game/51960/fifa-soccer-11/", "avgScore" => 4.7, "platforms" => ["Switch"] }], "completed" => [], "paused" => [], "dropped" => [] })
     end
-
-    # it "returns all games assosicated with correct genre (by Name)" do
-    #   token = JWT.encode({ user_id: 0, exp: 7.days.from_now.to_i }, Rails.application.secrets.secret_key_base)
-    #   post "/graphql", params: { query: queryByName(genre1.name) }, headers: { "Authorization" => "Bearer #{token}" }
-
-    #   json_response = JSON.parse(response.body)
-    #   games_response = json_response["data"]["getAllGamesByGenre"]
-
-    #   # Expect game_response to return "Persona 5
-    #   expect(games_response.count).to eq(1)
-    #   expect(games_response.find { |item| item["name"] == game1.name }).to_not be_nil  # "Persona 5"
-
-    #   # Expect game_response to not contain the games "FIFA 11" and "Assassin's Creed"
-    #   expect(games_response.find { |item| item["name"] == game3.name }).to be_nil # "Assassin's Creed"
-    #   expect(games_response.find { |item| item["name"] == game2.name }).to be_nil # "FIFA 11"
-    # end
 
     def queryByStatus
       <<~GQL
