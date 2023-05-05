12cf52f637a86a934ed9f432881ae382319b2f8a
Andrew Caruso <10090913+Yzma@users.noreply.github.com>
2023-05-05 10:11:33 -0400

    Modify allGames query to accept generes, platforms and tag filters

--- app/graphql/queries/game/get_all_games.rb
+++ app/graphql/queries/game/get_all_games.rb
@@ -4,8 +4,29 @@ module Queries
       description "Get all games"
       type [Types::Game::GameType], null: false
 
-      def resolve
-        ::Game.all
+      argument :platform, [String], required: false
+      argument :genre, [String], required: false
+      argument :tag, [String], required: false
+
+      def resolve(platform: nil, genre: nil, tag: nil)
+        allGames = ::Game.all
+
+        # Return games by platform if platform argument is provided
+        if (platform.present?)
+          allGames = allGames.joins(:platforms).where(platforms: { name: platform })
+        end
+
+        # Return games by genre if genre argument is provided
+        if (genre.present?)
+          allGames = allGames.joins(:genres).where(genres: { name: genre })
+        end
+
+        # Return games by tag if tag argument is provided
+        if (tag.present?)
+          allGames = allGames.joins(:tags).where(tags: { name: tag })
+        end
+
+        return allGames.uniq
       end
     end
   end
