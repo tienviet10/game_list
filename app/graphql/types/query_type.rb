b132fe872f2a15085f9f6333c89814e2e83909d4
Viet Tran <viettran101294@gmail.com>
2023-05-08 16:58:05 -0400

    Combine genres, paltforms, and tags together into 1 endpoint

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -4,11 +4,6 @@ module Types
     include GraphQL::Types::Relay::HasNodeField
     include GraphQL::Types::Relay::HasNodesField
 
-    # Example:
-    # field :users, resolver: Queries::Users::GetUsers
-    # field :posts, resolver: Queries::Posts::Posts
-    # field :games, resolver: Queries::Games::GamesByPlatform
-
     # Add root-level fields here.
     # They will be entry points for queries on your schema.
     field :all_games, resolver: Queries::Game::GetAllGames
@@ -18,6 +13,7 @@ module Types
     field :get_all_genres, resolver: Queries::Game::GetAllGenres
     field :get_all_platforms, resolver: Queries::Game::GetAllPlatforms
     field :get_all_tags, resolver: Queries::Game::GetAllTags
+    field :get_genres_platforms_tags, resolver: Queries::Game::GenresPlatformsTags
 
     field :get_all_games_by_platform, resolver: Queries::Game::GetAllGamesByPlatform
     field :get_all_games_by_genre, resolver: Queries::Game::GetAllGamesByGenre
