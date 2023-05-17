bab2e4775a4a2fe9e4d0c1b2f64cb40943652bf9
javascriptsucks <evix1988@live.cn>
2023-05-16 23:14:14 -0600

    create get_user_game_by_game_id query type

--- app/graphql/queries/user_games/get_user_game_by_game_id.rb
+++ app/graphql/queries/user_games/get_user_game_by_game_id.rb
@@ -2,9 +2,10 @@ module Queries
   module UserGames
     class GetUserGameByGameId < Queries::BaseQuery
       description "Get a user game by game id and user id (current user)"
-      type [Types::UserGame::UserGameType], null: false
 
       argument :game_id, ID, required: true
+
+      type [Types::UserGame::UserGameType], null: false
 
       def resolve(game_id:)
         UserGame.where(user_id: context[:current_user], game_id: game_id)
