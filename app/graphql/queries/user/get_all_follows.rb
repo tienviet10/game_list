d792de6e9d2a90a4faf6c05df9e4bbf517c075f1
javascriptsucks <evix1988@live.cn>
2023-06-02 18:12:07 -0600

    remove follow related fields from getUserById, instead use FollowType to return value

--- app/graphql/queries/user/get_all_follows.rb
+++ app/graphql/queries/user/get_all_follows.rb
@@ -2,13 +2,13 @@ module Queries
   module User
     class GetAllFollows < Queries::BaseQuery
       description "Get all follows for a user"
-      type [Types::User::FollowType], null: false
+      type Types::User::FollowType, null: false
 
       def resolve
         current_user = ::User.find_by(id: context[:current_user])
 
         if current_user
-          current_user.follows
+          current_user
         else
           raise GraphQL::ExecutionError, "User not authenticated"
         end
