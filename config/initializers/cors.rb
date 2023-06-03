b4fd446e87806e95bce3811515c74d0f34114df3
Viet Tran <viettran101294@gmail.com>
2023-06-03 18:17:06 -0400

    Added allowed host

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -17,6 +17,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
+    # origins "http://localhost:5173", "http://127.0.0.1:5173"
     origins "*"
     resource "*", headers: :any, methods: [:get, :post, :options]
   end
