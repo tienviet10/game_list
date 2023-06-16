547804e4b1edccb52bdf9093055628875448a905
javascriptsucks <evix1988@live.cn>
2023-06-16 01:00:32 -0600

    change in cors setting

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -17,8 +17,8 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "http://localhost:5173", "http://127.0.0.1:5173"
-    # origins "*"
+    # origins "http://localhost:5173", "http://127.0.0.1:5173"
+    origins "*"
     resource "*", headers: :any, methods: [:get, :post, :options]
   end
 end
