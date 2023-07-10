143b5630f8e4a712c9f5bb3c1267dad7f9f3866b
Andrew Caruso <10090913+Yzma@users.noreply.github.com>
2023-07-10 13:01:02 -0400

    Ignore GetGameById when checking jwt token

--- app/controllers/graphql_controller.rb
+++ app/controllers/graphql_controller.rb
@@ -49,7 +49,7 @@ class GraphqlController < ApplicationController
   end
 
   def verify_jwt_token
-    if params[:query].include?("login") || params[:query].include?("register") || params[:query].include?("IntrospectionQuery") || params[:query].include?("GetAllGames") || params[:query].include?("GetGameFilters")
+    if params[:query].include?("login") || params[:query].include?("register") || params[:query].include?("IntrospectionQuery") || params[:query].include?("GetAllGames") || params[:query].include?("GetGameFilters") || params[:query].include?("GetGameById")
       @current_user = nil
       return
     end
